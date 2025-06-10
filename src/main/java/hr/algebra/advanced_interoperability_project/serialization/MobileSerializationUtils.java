e7ce42623d12635421634c1e0e93025053da1c97
Matej Galic <matej.galic.ets@gmail.com>
2025-06-10 16:48:58 +0200

    refactored

--- src/main/java/hr/algebra/advanced_interoperability_project/serialization/MobileSerializationUtils.java
+++ src/main/java/hr/algebra/advanced_interoperability_project/serialization/MobileSerializationUtils.java
@@ -10,36 +10,47 @@ import java.util.Set;
 
 public class MobileSerializationUtils {
     private static final String FILE_NAME = "mobile.ser";
+    private static final HackerDTO HACKER_DTO = new HackerDTO("Trying to exploit");
 
     private MobileSerializationUtils() {}
 
     public static void serializeMobileToFile(MobileDTO mobileDTO) {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
-            //oos.writeObject(new HackerDTO("Trying to exploit"));
             oos.writeObject(mobileDTO);
+            //oos.writeObject(HACKER_DTO);
         } catch (IOException e) {
             throw new RuntimeException("Error occurred during serialization to " + FILE_NAME, e);
         }
     }
 
     public static MobileDTO deserializeMobileFromFileAndValidate() {
-        MobileDTO mobileDTO = null;
-
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
-            Object currentObject;
+            return readMobileDtoFromStream(ois);
+        } catch (IOException | ClassNotFoundException | DeserializationWhitelistException e) {
+            throw new IllegalStateException("Error deserializing mobile object", e);
+        }
+    }
 
-            for (currentObject = ois.readObject(); currentObject != null; currentObject = ois.readObject()) {
+    private static MobileDTO readMobileDtoFromStream(ObjectInputStream ois)
+            throws IOException,
+            ClassNotFoundException,
+            DeserializationWhitelistException {
+        MobileDTO mobileDTO = null;
+        boolean eofReached = false;
+
+        while (!eofReached) {
+            try {
+                Object currentObject = ois.readObject();
                 validateWhiteListedObjects(currentObject);
                 if (currentObject instanceof MobileDTO) {
                     mobileDTO = (MobileDTO) currentObject;
-                    break;
                 }
+            } catch (EOFException e) {
+                eofReached = true;
             }
-
-            return mobileDTO;
-        } catch (IOException | ClassNotFoundException | DeserializationWhitelistException e) {
-            throw new RuntimeException("Error deserializing mobile object", e);
         }
+
+        return mobileDTO;
     }
 
     private static void validateWhiteListedObjects(Object obj) throws DeserializationWhitelistException {
