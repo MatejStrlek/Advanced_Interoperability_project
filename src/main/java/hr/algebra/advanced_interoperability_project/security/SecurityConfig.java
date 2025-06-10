ef4c0f8b92c533fcca6b8d2c814d7506eed617c1
Matej <matej.galic.ets@gmail.com>
2025-06-10 20:27:13 +0200

    added fixed version of nosniff vulnerability in SecurityConfig.java

--- src/main/java/hr/algebra/advanced_interoperability_project/security/SecurityConfig.java
+++ src/main/java/hr/algebra/advanced_interoperability_project/security/SecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.security.web.header.writers.XXssProtectionHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -12,8 +15,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .headers(headers -> headers.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers ->
+                        headers.defaultsDisabled()
+                                .addHeaderWriter(new StaticHeadersWriter(
+                                        "X-Content-Type-Options", "nosniff"
+                                ))
+                        )
+                /*.headers(headers ->
+                        headers.contentTypeOptions(contentTypeOptionsConfig -> contentTypeOptionsConfig.disable()))
+                        */
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/rest/mobiles",
