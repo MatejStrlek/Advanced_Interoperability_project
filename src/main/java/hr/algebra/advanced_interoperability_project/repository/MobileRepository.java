59047d0bd073b43e063f82cc8b3d9e328e62f205
Matej <matej.galic.ets@gmail.com>
2025-06-12 14:43:59 +0200

    removed unused method in MobileJpa repo, and removed some white spaces

--- src/main/java/hr/algebra/advanced_interoperability_project/repository/MobileRepository.java
+++ src/main/java/hr/algebra/advanced_interoperability_project/repository/MobileRepository.java
@@ -3,8 +3,5 @@ package hr.algebra.advanced_interoperability_project.repository;
 import hr.algebra.advanced_interoperability_project.domain.Mobile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MobileRepository extends JpaRepository<Mobile, Long> {
-    List<Mobile> findMobileById (Long id);
 }
